4816f59732aeddcf29a81903a577c7f7cb7a2884
Girish Nanjappa <girish.nanjappa@gmail.com>
2025-11-14 19:59:25 +0530

    Agent code with Qwen3 model

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-from typing import List
-
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
+from langchain_ollama import ChatOllama
 
+@tool
+def search(query: str) -> str:
+    """
+    This is a tool that searches the web for a given query.
+    Arguments:
+        query: The search query.
+    Returns:
+        The search results.
+    """
+    print(f"Searching for: {query}")
+    return "Weather in Bengaluru is cloudy with a chance of rain."
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-
-    url: str = Field(description="The URL of the source")
-
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
-
-
-llm = ChatOpenAI(model="gpt-5")
-tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
-
+#llm = ChatOpenAI(model="gpt-4", temperature=0)
+#llm = ChatOllama(model="gemma3:270m", temperature=0.7)
+llm = ChatOllama(model="qwen3:4b", temperature=0.7)
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
 
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
-        }
-    )
-    print(result)
+    print("Hello, Agent")
+    agent_response = agent.invoke({"messages":HumanMessage(content="What's the weather like in Bengaluru?")})
+    print(f"Agent Response: {agent_response}")
 
 
 if __name__ == "__main__":
